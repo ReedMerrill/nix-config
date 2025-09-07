50d7bf6e98a65e81b8c9a5e805212653782981c1
Reed Merrill <reedjmerrill@gmail.com>
2025-09-06 19:28:48 -0600

    add gnome tiling shell

--- configuration.nix
+++ configuration.nix
@@ -67,6 +67,7 @@
     curl
     firefox
     git
+    gnomeExtensions.tiling-shell
     neovim
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
